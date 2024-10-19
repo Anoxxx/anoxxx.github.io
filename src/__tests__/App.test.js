f234e930edc6c1b69c0dc9d65b09f62bc92cc7ed
Zhengfei Zhang <trorooro@gmail.com>
2024-10-19 19:37:30 +0800

    update_241019_3

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Zhengfei Zhang");
+    expect(document.title).toBe('Zhengfei Zhang');
   });
 
   /*
